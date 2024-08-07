34884a23a30bdfc917c578f8abbe973ede0dc67b
Isuru Sandaruwan <sandaruwanisuru973@gmail.com>
2024-08-07 18:10:22 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'movie',
   plugins: [react()],
+  base: '/movies/',
+  build: {
+    outDir: 'dist',
+  },
 });
